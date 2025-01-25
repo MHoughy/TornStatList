d5019e7c34bde4d95f173808ced9573e01f0dece
MHoughy <Matthew-hough@hotmail.co.uk>
2025-01-25 15:56:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -221,10 +221,10 @@ function populateAPIKey() {
 function createTableRow(row, status, attackLink, index) {
   const isNotFirst = index > 0;
   const borderClass = isNotFirst ? 'border-t border-gray-200' : '';
-
+  
   return `
     <tr>
-      <td class="relative py-4 pl-4 pr-3 text-sm sm:pl-6 min-w-0 ${borderClass}">
+      <td class="relative py-4 pl-4 pr-3 text-sm sm:pl-6 min-w-0 ${borderClass} sticky">
         <div class="font-medium text-gray-900 dark:text-gray-300">
             <a href="https://www.torn.com/profiles.php?XID=${row.id}" target="_blank">
               ${row.name}
@@ -236,7 +236,7 @@ function createTableRow(row, status, attackLink, index) {
             <span>Total: ${row.BSP_total}</span>
         </div>
       </td>
-      <td class="lg:table-cell px-3 py-3.5 text-sm text-gray-500 dark:text-gray-300 min-w-0 ${borderClass}">${row.lvl}</td>
+      <td class="lg:table-cell px-3 py-3.5 text-sm text-gray-500 dark:text-gray-300 min-w-0 ${borderClass} sticky">${row.lvl}</td>
       <td class="lg:table-cell px-3 py-3.5 text-sm text-gray-500 dark:text-gray-300 min-w-0 ${borderClass}">${row.BSP_total}</td>
       <td class="lg:table-cell px-3 py-3.5 text-sm text-gray-500 dark:text-gray-300 min-w-0 ${borderClass}">${row.total}</td>
       <td class="lg:table-cell px-3 py-3.5 text-sm text-gray-500 dark:text-gray-300 min-w-0 ${borderClass}">${row.str}</td>
@@ -248,12 +248,13 @@ function createTableRow(row, status, attackLink, index) {
         <div class="lg:hidden w-40">${status}</div>
         <div class="hidden lg:block w-40">${status}</div>
       </td>
-      <td class="relative py-3.5 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 min-w-0 ${borderClass}">
+      <td class="relative py-3.5 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 min-w-0 ${borderClass} sticky">
         ${attackLink}
       </td>
     </tr>
   `;
 }
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
